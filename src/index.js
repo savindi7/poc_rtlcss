fc06aec22b42d039c5dd58fd4587a64094345505
savindi7 <savindi@wso2.com>
2024-12-13 20:45:40 +0530

    update css links

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// src/index.js
-import './assets/theme.min.css'; // Import the LTR CSS
-import './assets/theme.rtl.min.css'; // Import the RTL CSS
+const isRTL = true; // Example condition
+const stylesheetPath = isRTL 
+    ? '../dist/assets/theme.rtl.min.css' 
+    : '../dist/assets/theme.min.css';
 
-window.onload = function () {
-    // For example, check the browser's language setting
-    const isRTL = true
-
-    // If language is Arabic or other RTL language, apply RTL stylesheet
-    if (isRTL) {
-        document.getElementById('theme-stylesheet').setAttribute('href', 'assets/theme.rtl.min.css');
-    } else {
-        // Keep LTR stylesheet if language is not Arabic
-        document.getElementById('theme-stylesheet').setAttribute('href', 'assets/theme.min.css');
-    }
-};
+document.getElementById('theme-stylesheet').setAttribute('href', stylesheetPath);
